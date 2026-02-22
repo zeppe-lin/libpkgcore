2ee85d29f92e3f88f68734d20cba999b0ad18455
Alexandr Savca <alexandr.savca89@gmail.com>
2026-02-23 00:10:11 +0200

    headers: refine libpkgcore umbrella header

--- src/libpkgcore/include/libpkgcore.h
+++ src/libpkgcore/include/libpkgcore.h
@@ -1,13 +1,20 @@
 /*!
- * \file libpkgutils.h
+ * \file libpkgcore.h
+ * \brief Umbrella header for libpkgcore public API.
+ *
+ * Including this header pulls in all public interfaces:
+ *   - db_lock: RAII database locking
+ *   - fs_utils: filesystem helpers
+ *   - pkgutil: package database and install/remove logic
+ *
+ * Consumers may alternatively include individual headers from
+ * <pkgcore/...> for finer granularity.
+ *
  * \copyright See COPYING for license terms and COPYRIGHT for notices.
  */
 
-#ifndef LIB_PKG_UTILS_H
-#define LIB_PKG_UTILS_H
+#pragma once
 
-#include "db_lock.h"
-#include "fs_utils.h"
-#include "pkgutil.h"
-
-#endif // LIB_PKG_UTILS
+#include "pkgcore/db_lock.h"
+#include "pkgcore/fs_utils.h"
+#include "pkgcore/pkgutil.h"
